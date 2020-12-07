f3966e758c96b65273107356bfc9103c50a7b732
TheShiferawShow <68077955+TheShiferawShow@users.noreply.github.com>
2020-12-06 22:09:08 -0500

    More Minor Edits

--- Vote_percentage.py
+++ Vote_percentage.py
@@ -1,3 +1,12 @@
 def vote_percentage(results):
     """(str)-> (float)
     Precondition: all values within the string are either abstained, yes or no"""
+    results = results.split()
+    # determine length of input
+    length_ofinput = (len(results))
+    # determine number of abstained within the input
+    recurrance_ofabstained = results.count('abstained')
+    recurrance_ofyes = results.count('yes')
+    number_ofyesno = length_ofinput-recurrance_ofabstained
+    final = recurrance_ofyes/number_ofyesno
+    return final
